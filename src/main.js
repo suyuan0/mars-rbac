51ca472b6d9def0dedfef296f7ed9218396c3c72
培培 <614963845@qq.com>
2022-07-07 16:42:50 +0800

    chore(ant-dsign): 引入ant-dsign组件 & reset样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'ant-design-vue/dist/antd.css'
+import Antd from 'ant-design-vue'
+import './styles/reset.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(Antd)
+app.use(store).use(router).mount('#app')
